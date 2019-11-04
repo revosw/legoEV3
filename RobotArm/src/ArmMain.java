26661bac5dd05032c2b9e1ff25acb20f0fdb473a
Olav Valle <olavval@stud.ntnu.no>
2019-11-04 22:22:20 +0100

    add RobotArm module, and behavior methods

---
+++ RobotArm/src/ArmMain.java
@@ -0,0 +1,25 @@
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
+
+import java.awt.*;
+
+public class ArmMain {
+
+    public static void main(String[] args)
+    {
+        // Behaviors
+        Behavior sense = new SenseColour();
+        Behavior white = new MoveWhite();
+        Behavior black = new MoveBlack();
+        Behavior back = new MoveBack();
+
+        // Behavior array
+        Behavior [] bArray = {sense, white, black, back};
+
+        // Arbitrator
+        Arbitrator arb = new Arbitrator(bArray);
+        arb.go();
+
+    }
+
+}
---
+++ RobotArm/src/SenseColour.java
@@ -0,0 +1,18 @@
+import lejos.robotics.subsumption.Behavior;
+
+public class SenseColour implements Behavior {
+    @Override
+    public boolean takeControl() {
+        return false;
+    }
+
+    @Override
+    public void action() {
+
+    }
+
+    @Override
+    public void suppress() {
+
+    }
+}
